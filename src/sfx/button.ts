8adfa4874e01643b7c67c7131a7ceffd91c3d2df
Chris Swithinbank <swithinbank@gmail.com>
2025-04-21 12:19:07 +0200

    Format

--- src/sfx/button.ts
+++ src/sfx/button.ts
@@ -1,5 +1,5 @@
-import { Howl } from 'howler';
-import sprite from './button-sprite.mp3';
+import { Howl } from "howler";
+import sprite from "./button-sprite.mp3";
 
 /** The number of distinct button click samples stored in the sound sprite. */
 const sampleCount = 11;
@@ -12,24 +12,24 @@ let lastSampleId: string | undefined;
 
 /** Get a random ID for one of the sounds in the sound sprite. */
 const getNextSampleId = (): string => {
-	const nextSampleId = String(Math.floor(Math.random() * sampleCount));
-	// Avoid repeating the same sample as the last click.
-	if (nextSampleId === lastSampleId) return getNextSampleId();
-	lastSampleId = nextSampleId;
-	return nextSampleId;
+  const nextSampleId = String(Math.floor(Math.random() * sampleCount));
+  // Avoid repeating the same sample as the last click.
+  if (nextSampleId === lastSampleId) return getNextSampleId();
+  lastSampleId = nextSampleId;
+  return nextSampleId;
 };
 
 /** A Howler instance that plays back button clicks. */
 const buttonSfx = new Howl({
-	src: sprite,
-	// This code tells Howler to break up the sprite file into 375ms chunks.
-	sprite: Object.fromEntries(
-		Array.from({ length: sampleCount }).map((_, index) => [
-			index,
-			[index * sampleDuration, sampleDuration],
-		])
-	),
-	volume: 0.5,
+  src: sprite,
+  // This code tells Howler to break up the sprite file into 375ms chunks.
+  sprite: Object.fromEntries(
+    Array.from({ length: sampleCount }).map((_, index) => [
+      index,
+      [index * sampleDuration, sampleDuration],
+    ]),
+  ),
+  volume: 0.5,
 });
 
 /** Play a random plastic button click sound effect. */
